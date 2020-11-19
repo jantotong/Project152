51c86a32f53f9341fbef7b20e8c7e4a3f2026dd6
jantotong <43649573+jantotong@users.noreply.github.com>
2020-11-19 10:46:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import request
 
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("record.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    word = request.args.get('word')
+    trial_number = request.args.get('trialN')
+    name = request.args.get('name')
+    
+    return
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=80)
